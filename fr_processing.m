7a9f654dc383c71aec743deda3493e26815f3e97
Alexandre Camuto <alexandrecamuto@dyn1207-137.wlan.ic.ac.uk>
2017-02-28 17:26:13 +0000

    CVM-22
    improved organisation of PSTH gen and polar generation

--- fr_processing.m
+++ fr_processing.m
@@ -1,9 +1,13 @@
-function [l_PSTH_non_shifted,l_PSTH_shifted,l_local_shifted, shifted_signal,Av_po] = fr_processing(trial,dt)
+function output = fr_processing(trial,dt)
 
-l_PSTH_shifted = cell(98,8); 
-l_PSTH_non_shifted = cell(98,8); 
-l_local_shifted = cell(98,8); 
-shifted_signal = cell(98,8); 
+output = struct();
+
+output.l_PSTH_non_shifted = cell(98,8);
+output.l_PSTH_shifted = cell(98,8);
+output.shifted_signal = cell(98,8);
+output.l_local_shifted = cell(98,8);
+output.Av_po = cell(1,8);
+
 
 for n=1:size(trial,1)
     for a = 1:size(trial,2)
@@ -17,15 +21,15 @@ end
 for i = 1:size(trial(1,1).spikes,1)
      for a = 1:size(trial,2)
 
-l_PSTH_non_shifted{i,a} = zeros(1, max(max(max(sizes_recordings(:,:,:))))); 
+output.l_PSTH_non_shifted{i,a} = zeros(1, max(max(max(sizes_recordings(:,:,:))))); 
 
 A = zeros(1,max(max(max(sizes_recordings(:,:,:)))));
 B = zeros(1,max(max(max(sizes_recordings(:,:,:)))));
 
          
 closest_trial = findFirstOnset(trial,i,a);
-l_local_shifted{i,a} = zeros(size(trial,1), max(max(max(sizes_recordings(:,:,:)))));
-shifted_signal{i,a} = zeros(size(trial,1), max(max(max(sizes_recordings(:,:,:)))));
+output.l_local_shifted{i,a} = zeros(size(trial,1), max(max(max(sizes_recordings(:,:,:)))));
+output.shifted_signal{i,a} = zeros(size(trial,1), max(max(max(sizes_recordings(:,:,:)))));
 
 for n = 1:size(trial,1)
     
@@ -38,32 +42,31 @@ A(1:len) = A(1:len) + new_shift;
 B(1:len) = B(1:len) + trial(n,a).spikes(i,:);
 
 
-l_local_shifted{i,a}(n,1:length(fr_es(new_shift,dt))) = fr_es(new_shift,dt);
+output.l_local_shifted{i,a}(n,1:length(fr_es(new_shift,dt))) = fr_es(new_shift,dt);
 
-shifted_signal{i,a}(n,1:length(new_shift)) = new_shift; 
+output.shifted_signal{i,a}(n,1:length(new_shift)) = new_shift; 
 
 B(1:len) = B(1:len) + trial(n,a).spikes(i,:);
 
 end 
 
-l_PSTH_shifted{i,a} = fr_es(A,dt);
-l_PSTH_non_shifted{i,a} = fr_es(B,dt);
+output.l_PSTH_shifted{i,a} = fr_es(A,dt);
+output.l_PSTH_non_shifted{i,a} = fr_es(B,dt);
 
 
      end 
 end 
 
 
-Av_po = cell(1,8);
 
     for angle = 1:8
-Av_po{angle} = zeros(3,max(max(max(sizes_recordings(:,:,:)))));
+output.Av_po{angle} = zeros(3,max(max(max(sizes_recordings(:,:,:)))));
         for n = 1:100
             
-Av_po{angle}(:,1:size(trial(n,angle).handPos,2)) = Av_po{angle}(:,1:size(trial(n,angle).handPos,2)) + trial(n,angle).handPos;
+output.Av_po{angle}(:,1:size(trial(n,angle).handPos,2)) = output.Av_po{angle}(:,1:size(trial(n,angle).handPos,2)) + trial(n,angle).handPos;
 
         end 
-        Av_po{angle} = Av_po{angle}./100;
+        output.Av_po{angle} = output.Av_po{angle}./100;
     end 
 
 
