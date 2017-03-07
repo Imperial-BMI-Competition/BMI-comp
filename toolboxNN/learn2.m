666cacff4063c884c7f2ead7bdff4267845b29b2
Jonathan Campbell <jonathan.o.campbell@gmail.com>
2017-03-07 12:57:22 +0000

    added preprocessing to toolbox RNN

--- toolboxNN/learn2.m
+++ toolboxNN/learn2.m
@@ -3,29 +3,37 @@ clearvars;
 close all;
 fprintf('Loading data...\n')
 load('monkeydata_training.mat');
-data_train = trial(1:90,:);
-data_test = trial(91:end,:);
+% for unitNum = 1:98
+%     for dirNum = 1:8
+%         for trialNum = 1:100
+%             spikeTrain = trial(trialNum,dirNum).spikes(unitNum,:);
+%             for timeIdx = length(spikeTrain)
+%                 collate(trialNum,timeIdx) = spikeTrain(timeIdx);
+%             end
+%         end
+%         mean(collate,1)
+%         avgSpikecounts{unitNum,dirNum} = mean(collate,1);
+%         clearvars collate
+%     end
+% end
+% for n=1:size(trial,1)
+%     for a = 1:size(trial,2)
+%     positions = trial(n,a).handPos;
+%     [theta, rho] = cart2pol(positions(1,:),positions(2,:));
+%     trial(n,a).handPosPolar = [theta; rho];
+%     end
+% end
+output = fr_processing(trial,20);
 max_D = 8;
 hidden = 4;
 delays = 2;
-wdw = 100;
-sigma = 20;
-neurons = 98;
 fprintf('Initialising model...\n')
 model = layrecnet(1:delays,hidden,'traingdx');
 model.trainParam.epochs = 20;
-model.trainParam.showWindow = 1;
-showFig = 1;
 %% Training
-j = 1;
-AEX = zeros(1,size(data_train,1));
-AEY = zeros(1,size(data_train,1));
-for N = 1:size(data_train,1)
-    EX = zeros(1,max_D);
-    EY = zeros(1,max_D);
-    for D = 1:max_D
-        fprintf(strcat('Trial:',num2str(N),',Dir:',num2str(D),'\n'))
-        X = con2seq(g_filter(data_train(N,D).spikes(1:neurons,:),wdw,sigma));
+    for D = 1:8
+        fprintf(strcat('Dir:',num2str(D),'\n'))
+        X = con2seq(output.l_PSTH_non_shifted);
         T = con2seq(data_train(N,D).handPos(1:2,:));
         [Xs,Xi,Ai,Ts] = preparets(model,X,T);
         fprintf('Training model...\n')
@@ -46,19 +54,7 @@ for N = 1:size(data_train,1)
             polarplot(theta,rho,'b')
             hold off;
         end
-        EX(D) = mean((DT(1,delays+1:end)-DY(1,:)).^2);
-        EY(D) = mean((DT(2,delays+1:end)-DY(2,:)).^2);
     end
-    if showFig == 1
-        figure(2)
-        subplot(1,2,1)
-        AEX(N) = mean(EX);
-        plot(AEX(1:N),'rx-')
-        subplot(1,2,2)
-        AEY(N) = mean(EY);
-        plot(AEY(1:N),'rx-')
-    end
-end
 %% Testing
 close all
 TEX = zeros(1,size(data_test,1));
@@ -66,7 +62,7 @@ TEY = zeros(1,size(data_test,2));
 fprintf('Testing model...\n')
 for N = 1:size(data_test,1)
     D = randi(8);
-    X = con2seq(g_filter(data_test(N,D).spikes(1:neurons,:),wdw,sigma));
+    X = con2seq(data_test(N,D).spikes(1:neurons,:));
     T = con2seq(data_test(N,D).handPos(1:2,:));
     [Xs,Xi,Ai,Ts] = preparets(model,X,T);
     Y = model(Xs,Xi,Ai);
