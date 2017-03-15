5e7750ec9c367ffdebb88c122f03dea3b88cf93d
unknown <joc13@rsmwsg08-084.ic.ac.uk>
2017-03-15 15:51:15 +0000

    Moved files

---
+++ multiRNN/main.m
@@ -0,0 +1,80 @@
+RMSE = [];
+
+% Jonathan Campbell, Alexander Camuto, Enrico Tosoratti, Amna Askari
+
+% (try not to edit this file too much - based off the file
+% testFunction_for_students_MTb(1).m which assesses the performance of the
+% model)
+
+
+%% Continuous Position Estimator Test Script
+% This function first calls the function "positionEstimatorTraining" to get
+% the relevant modelParameters, and then calls the function
+% "positionEstimator" to decode the trajectory.
+
+close all
+load monkeydata_training.mat
+
+% Set random number generator
+rng(4832932);
+ix = randperm(length(trial));
+
+% Select training and testing data (you can choose to split your data in a different way if you wish)
+trainingData = trial(ix(1:90),:);
+testData = trial(ix(91:end),:);
+
+
+
+meanSqError = 0;
+n_predictions = 0;  
+
+figure
+hold on
+axis square
+grid
+
+% Train Model
+fprintf('Training the continuous position estimator...')
+modelParameters = positionEstimatorTraining(trainingData);
+
+%% Test model
+fprintf('Testing the continuous position estimator...')
+for tr=1:size(testData,1)
+    display(['Decoding block ',num2str(tr),' out of ',num2str(size(testData,1))]);
+    pause(0.001)
+    for direc=1 
+        decodedHandPos = [];
+
+        times=320:20:size(testData(tr,direc).spikes,2);
+        
+        for t=times
+            past_current_trial.trialId = testData(tr,direc).trialId;
+            past_current_trial.spikes = testData(tr,direc).spikes(:,1:t); 
+            past_current_trial.decodedHandPos = decodedHandPos;
+
+            past_current_trial.startHandPos = testData(tr,direc).handPos(1:2,1); 
+            
+            if nargout('positionEstimator') == 3
+                [decodedPosX, decodedPosY, newParameters] = positionEstimator(past_current_trial, modelParameters);
+                modelParameters = newParameters;
+            elseif nargout('positionEstimator') == 2
+                [decodedPosX, decodedPosY] = positionEstimator(past_current_trial, modelParameters);
+            end
+            
+            decodedPos = [decodedPosX; decodedPosY];
+            decodedHandPos = [decodedHandPos decodedPos];
+            
+            meanSqError = meanSqError + norm(testData(tr,direc).handPos(1:2,t) - decodedPos)^2;
+            
+        end
+        n_predictions = n_predictions+length(times);
+        hold on
+        plot(decodedHandPos(1,:),decodedHandPos(2,:), 'r');
+        plot(testData(tr,direc).handPos(1,times),testData(tr,direc).handPos(2,times),'b')
+    end
+end
+
+
+legend('Decoded Position', 'Actual Position')
+
+RMSE = sqrt(meanSqError/n_predictions)
---
+++ multiRNN/positionEstimator.m
@@ -0,0 +1,21 @@
+function [decodedPosX, decodedPosY] = positionEstimator(data_test, model)
+function output = g_filter(spikes, wdw, sigma)
+output = zeros(size(spikes));
+    for n = 1:size(spikes,1)
+        a = 1/(sigma*sqrt(2*pi));
+        x = -(wdw-1)/2:(wdw-1)/2;
+        g = a*exp(-(x.^2)./(2*sigma^2));
+        g_spikes = conv(spikes(n,:),g);
+        output(n,:) = g_spikes(wdw/2:end-wdw/2);
+    end
+end
+wdw = 400;
+sigma = 100;
+X = con2seq(g_filter(data_test.spikes,wdw,sigma));
+[Xs,Xi,Ai,~] = preparets(model.net,X);
+Dpos = seq2con(model.net(Xs,Xi,Ai));
+DposC = sum(Dpos{:},2);
+decodedPosX = DposC(1) + model.x0;
+decodedPosY = DposC(2) + model.y0;
+end
+
